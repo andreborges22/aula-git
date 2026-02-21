f2ce0406070af1bf2593150c44a0cfd2ccc0ee94
andreborges22 <andreborges22@gmail.com>
2026-02-21 08:55:44 -0300

    modifiquei as classes Veiculo e carro

--- Veiculo.py
+++ Veiculo.py
@@ -1,2 +1,5 @@
-# desenvolver a class veiculo
-# escrever o método dirigir
+# criar a class veiculo
+class Veiculo():
+    # escrever o método dirigir
+    def dirigir():
+        return "O veiculo está em movimento"
