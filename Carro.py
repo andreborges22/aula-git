e9edba80f169acf9b441c557127d9a6954b18b9b
andreborges22 <andreborges22@gmail.com>
2026-02-21 09:04:35 -0300

    Alterei a classe carro

--- Carro.py
+++ Carro.py
@@ -1,6 +1,6 @@
-#criar a classe carro que herda de veículo
+# criar a classe carro que herda de veículo
 
 class Carro(Veiculo):
-#sobreescrever o metodo dirigir
+    # sobreescrever o metodo dirigir
     def dirigir():
         return "O carro está em movimento"
