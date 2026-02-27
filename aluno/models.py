737b4e76a5fa6cb754a0b9d519a5256816762f12
andreborges22 <andreborges22@gmail.com>
2026-02-26 23:16:55 -0300

    Projeto teste com crud de aluno funcionando

--- aluno/models.py
+++ aluno/models.py
@@ -7,7 +7,8 @@ from curso.models import Curso
 class Aluno(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField(max_length=100, default="sem@email.com")
-    curso = models.ForeignKey(Curso,on_delete=models.PROTECT)
+    nascimento = models.DateField(null=True)
+    curso = models.ForeignKey(Curso, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
--- aluno/views.py
+++ aluno/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render, redirect
-from .models import Aluno
+from .models import Aluno,Curso
 # Create your views here.
 
 
 def home(request):
     alunos = Aluno.objects.all()
-    return render(request, "aluno/home.html", {"alunos": alunos})
+    cursos = Curso.objects.all()
+    return render(request, "aluno/home.html", {"alunos": alunos,"cursos": cursos})
 
 
 def cadastrar(request):
     nome = request.POST.get("nome")
     email = request.POST.get("email")
-    Aluno.objects.create(nome=nome, email=email)
+    curso_id = request.POST.get("curso")
+    Aluno.objects.create(nome=nome, email=email,curso_id = curso_id)
     return redirect(home)
 
 
 def editar(request, id):
     aluno = Aluno.objects.get(id=id)
-    return render(request, "aluno/update.html", {"aluno": aluno})
+    cursos = Curso.objects.all()
+    return render(request, "aluno/update.html", {"aluno": aluno,"cursos":cursos})
 
 
 def atualizar(request, id):
     nome = request.POST.get("nome")
     email = request.POST.get("email")
+    curso_id = request.POST.get("curso")
     aluno = Aluno.objects.get(id=id)
     aluno.nome = nome
     aluno.email = email
+    aluno.curso_id = curso_id
     aluno.save()
     return redirect(home)
 
